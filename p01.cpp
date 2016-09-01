2f88a32cde674191ae75a6b46adc743ff6d8fd7d
gFleka <nkrizanec@riteh.hr>
2016-09-01 03:47:43 +0200

    Almost Working version
    Added more functionality to "lib" now iterates all of the benchmarks,
    still need to add function reference to call it from benchmark tool and
    add cout the statistics

--- p01.cpp
+++ p01.cpp
@@ -6,6 +6,7 @@
 #include <ratio>
 #include <chrono>
 
+using namespace std::chrono;
 
 struct benchmark_test{
  std::string title;
@@ -14,28 +15,52 @@ struct benchmark_test{
 
 class benchmark
 {
+  
+  
 private:
   std::list<benchmark_test> benchmarks_list;
+
+public:
+  void add(std::string title, int repetitions) //TODO Dodati ovdje da kao argument prima funkciju koja se onda može pozivat i izvršavati
+  {
+    benchmark_test temp;
+    temp.title=title;
+    temp.repetitions=repetitions;
+    benchmarks_list.push_back(temp);    
+  }
   
+  void remove(std::string title)
+  {
+    //TODO Add remove method to remove single benchmarks from the test
+  }
+  
+  void run(){
+    for( auto & ibenchmark : benchmarks_list )
+    {
+      high_resolution_clock::time_point t1 = high_resolution_clock::now();
+      std::cout << "Test Name: " << ibenchmark.title << std::endl;
+      std::cout << "printing out" << ibenchmark.repetitions << " stars...\n";
+      for (int i=0; i<ibenchmark.repetitions; ++i) std::cout << "*";
+      std::cout << std::endl;
+
+      high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+      duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+
+      std::cout << "It took me " << time_span.count() << " seconds.";
+      std::cout << std::endl;
+    }
+  }
 };
 
 int main ()
 {
-  using namespace std::chrono;
+  benchmark testBenchmark;
+  testBenchmark.add("++Prvi Test Naziv++",20);
+  testBenchmark.run();
   
 
-  high_resolution_clock::time_point t1 = high_resolution_clock::now();
-
-  std::cout << "printing out 10000 stars...\n";
-  for (int i=0; i<10000; ++i) std::cout << "*";
-  std::cout << std::endl;
-
-  high_resolution_clock::time_point t2 = high_resolution_clock::now();
-
-  duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-
-  std::cout << "It took me " << time_span.count() << " seconds.";
-  std::cout << std::endl;
+  
 
   return 0;
 }
